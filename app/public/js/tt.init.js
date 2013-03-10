d8e5694a9cb759d629200aa2b397525a99168878
Andrew Childs <ac@glomerate.com>
2013-03-10 18:32:13 -0400

    AC: Add Google Analytics tag to demo install

--- app/public/js/tt.init.js
+++ app/public/js/tt.init.js
@@ -499,9 +499,24 @@ TT.Init = (function () {
     });
   };
 
+  pub.loadGoogleAnalytics = function () {
+    window._gaq = window._gaq || [];
+    window._gaq.push(['_setAccount', 'UA-39159096-1']);
+    window._gaq.push(['_trackPageview']);
+
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') +
+      '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  };
+
   pub.init = function () {
     if (pub.firstRun) {
       TT.View.drawPageLayout();
+      pub.loadGoogleAnalytics();
     } else {
       $.each(TT.Model, function (index, model) {
         if (model.flush) {
